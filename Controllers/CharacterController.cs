d49e1526ccfa9a3723038af676ef4fd64fb45c54
Harjot Singh <harjot.singh@nri-distribution.com>
2021-03-03 22:43:19 -0800

    post request

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -30,5 +30,17 @@ namespace dotnetcore.Controllers
         {
             return Ok(characters);
         }
+
+        [HttpPost]
+        public IActionResult PostCharacter(Character character)
+        {
+            if (string.IsNullOrEmpty(character.Name) || character.Id < 1) return BadRequest("Id cannot be zero or negative number. Name is required.");
+            if (characters.Any(c => c.Id == character.Id)) return BadRequest("Character with this Id already exists.");
+            if (characters.Any(c => c.Name == character.Name)) return BadRequest("Character with this Name already exists.");
+
+            characters.Add(character);
+            
+            return Ok($"Character {character.Name} added.");
+        }
     }
 }
